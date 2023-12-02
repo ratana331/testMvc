1530387b1e99bb1ad4b9c512bf42d071a605f4d9
ratana331 <racharatana@gmail.com>
2023-12-02 09:34:13 +0700

    sec

---
+++ Controllers/DocsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testMvc.Controllers
+{
+    public class DocsController : Controller
+    {
+        // GET: Docs
+        public FileResult Show()
+        {
+            return File(Server.MapPath("-/Content/myFile.txt"), "text/plain");
+        }
+        public FileResult Download()
+        {
+            return File(Server.MapPath("-/Content/myFile.txt"), "text/plain", "MVC5.txt");
+        }
+    }
+}
